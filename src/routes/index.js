0e63b77f28cfc2dc8a3040cb494efb703918d76a
Romulescu Florin <romulescu.florin.dev@gmail.com>
2023-07-23 12:07:57 +0300

    GET Fise de lucru
    Am adaugat un get pentru pagina de fise de lucru

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', evenimente: true});
 });
 
+
+/* GET Fise de lucru page. */
+router.get('/fise-de-lucru', (req, res, next) => {
+  // TODO change index with the name of the ejs file
+  res.render('index', { title: 'Fise de lucru', fiseDeLucru: true})
+})
+
 module.exports = router;
